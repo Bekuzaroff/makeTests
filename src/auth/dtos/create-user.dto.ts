c8bfb4f23879af1200df1ae0e9550420cf4f4ca3
= <bekuzarovchermen@gmail.com>
2025-04-25 23:09:32 +0300

    Revert "update create_user"
    This reverts commit 9519b3e506b84cdf56f04fafc8ab6babcbb860f0.

--- src/auth/dtos/create-user.dto.ts
+++ src/auth/dtos/create-user.dto.ts
@@ -5,10 +5,7 @@ export class CreateUserDto{
     id: number
 
     @IsString({message: 'nick_name should be a string'})
-<<<<<<< HEAD
     @MinLength(6, {message: 'your nick name must be more than 6 characters'})
-=======
->>>>>>> ea2e62f7bd05fa9383f403e0c827cb74e4940891
     nick_name: string
 
     @IsEmail()
@@ -16,9 +13,6 @@ export class CreateUserDto{
     
     @MinLength(6, {message: 'your password must be more than 6 characters'})
     password: string
-<<<<<<< HEAD
 
     confirm_password: string
-=======
->>>>>>> ea2e62f7bd05fa9383f403e0c827cb74e4940891
 }
